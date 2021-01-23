83c1a811fa52fd202bbb1614b0c039b4f67e7b6b
Furcht968 <tomu.iwanaga129+github@gmail.com>
2021-01-23 22:43:03 +0900

    add: dart simple package追加、description, README.md編集

---
+++ example/TimeDart_example.dart
@@ -0,0 +1,6 @@
+import 'package:TimeDart/TimeDart.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/TimeDart.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library TimeDart;
+
+export 'src/TimeDart_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
